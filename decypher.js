4e27158106640bcf1f608460ba4fa2ab392d2395
D-Savel <dad.savel@gmail.com>
2021-02-17 18:27:57 +0100

    Modification variable decalageCodeCesar et compteur pour afficher solution pour décalage de 1 à 10

--- decypher.js
+++ decypher.js
@@ -1,18 +1,18 @@
-let strCesar = 'YRMV PFL VMVI YRU R UIVRD EVF KYRK PFL NVIV JF JLIV NRJ IVRC NYRK ZW PFL NVIV LERSCV KF NRBV WIFD KYRK UIVRD YFN NFLCU PFL BEFN KYV UZWWVIVETV SVKNVVE KYV UIVRD NFICU REU KYV IVRC NFICU'
-let valeurDecalage = -17
-
-let decodeStrCesar = ''
-for (let index = 0; index < strCesar.length; index++) {
-  if (strCesar[index] === ' ') {
-    decodeStrCesar += ' '
-  }
-  else {
-    let asciiStrCesar = strCesar.charCodeAt(index)
-    let newAscii = asciiStrCesar + valeurDecalage
-    while (newAscii < 65) {
-      newAscii += 26
+et strCesar = 'YRMV PFL VMVI YRU R UIVRD EVF KYRK PFL NVIV JF JLIV NRJ IVRC NYRK ZW PFL NVIV LERSCV KF NRBV WIFD KYRK UIVRD YFN NFLCU PFL BEFN KYV UZWWVIVETV SVKNVVE KYV UIVRD NFICU REU KYV IVRC NFICU'
+for (let valeurDecalage = 0; valeurDecalage < 11; valeurDecalage++) {
+  let decodeStrCesar = ''
+  for (let index = 0; index < strCesar.length; index++) {
+    if (strCesar[index] === ' ') {
+      decodeStrCesar += ' '
     }
-    decodeStrCesar += String.fromCharCode(newAscii)
+    else {
+      let asciiStrCesar = strCesar.charCodeAt(index)
+      let newAscii = asciiStrCesar + valeurDecalage
+      while (newAscii > 90) {
+        newAscii -= 26
+      }
+      decodeStrCesar += String.fromCharCode(newAscii)
+    }
   }
+  console.log(`Pour un décalage de ${valeurDecalage} :\n ${decodeStrCesar} \n`)
 }
-console.log(decodeStrCesar)
