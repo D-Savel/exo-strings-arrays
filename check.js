c369edbe3e5eb958a029411323e618f364ac5fd9
D-Savel <dad.savel@gmail.com>
2021-02-18 09:52:01 +0100

    Update check.js code : toLowerCase for all string

--- check.js
+++ check.js
@@ -1,6 +1,6 @@
 let str = 'JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript'
 let wordToFind = 'Javascript'
-if (str.toLowerCase().includes(wordToFind)) {
+if (str.toLowerCase().includes(wordToFind.toLowerCase())) {
   console.log(`Le mot ${wordToFind} est dans la string "${str}"`)
 }
 else {
